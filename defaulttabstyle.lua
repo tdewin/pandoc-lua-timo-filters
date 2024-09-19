8238eda557450e715b44471d9673321b448e2d64
tdewin <tdewin@users.noreply.github.com>
2024-09-19 12:08:40 +0200

    Create defaulttabstyle.lua

---
+++ defaulttabstyle.lua
@@ -0,0 +1,26 @@
+-- run with                                                                                                                                                                 
+-- -L defaulttabstyle.lua -M tabstyle:mystyle
+-- set a custom tab style for docx output
+-- only works with pandoc 3.4>
+                                                                                                                                                                            
+local tabstyle = "table"                                                                                                                                                    
+                                                                                                                                                                            
+function Meta(meta)                                                                                                                                                         
+        if meta.tabstyle ~= nil then                                                                                                                                        
+                tabstyle = meta.tabstyle                                                                                                                                    
+        end                                                                                                                                                                 
+                                                                                                                                                                            
+end                                                                                                                                                                         
+                                                                                                                                                                            
+function Table(elem)                                                                                                                                                        
+  print(tabstyle)                                                                                                                                                           
+  if elem.attr.attributes["custom-style"] == nil then                                                                                                                       
+    elem.attr.attributes["custom-style"] = tabstyle                                                                                                                         
+  end                                                                                                                                                                       
+  return elem                                                                                                                                                               
+end                                                                                                                                                                         
+                                                                                                                                                                            
+return {                                                                                                                                                                    
+  {Meta = Meta},                                                                                                                                                            
+  {Table = Table}                                                                                                                                                           
+} 
