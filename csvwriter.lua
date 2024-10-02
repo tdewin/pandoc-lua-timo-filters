f6f1f462502ea6d87843b5260f3ef0c9f1bbc3e7
tdewin <tdewin@users.noreply.github.com>
2024-10-02 17:34:02 +0200

    Update csvwriter.lua

--- csvwriter.lua
+++ csvwriter.lua
@@ -1,16 +1,32 @@
 -- some crude way to export to csv
--- keeps the headers intact and just put them in csv code blocks
--- use with pandoc <input> -t csvwriter.lua -o csvembed.md
+-- csv writer
+-- use with pandoc <input> -t csvwriter.lua -o output.csv
+
+function escape(text)
+    ln = {}
+    for c in text:gmatch"." do
+        if c == "\"" then
+            --If double-quotes are used to enclose fields, then a double-quote appearing inside a field must be escaped by preceding it with another double quot
+            table.insert(ln,c)
+            table.insert(ln,c)
+        else
+            table.insert(ln,c)
+        end
+    end
+    return "\""..table.concat(ln,"").."\""
+end
+
 function Writer (doc, opts)
+    local flatified = {}
+
     --https://pandoc.org/lua-filters.html#type-table
     local filter = {
       Table = function (tb)
-          local flatified = {}
           for i, r in ipairs(tb["head"]["rows"]) do
                 local flatifiedrow = {}
                 for k,c in ipairs(r["cells"]) do
-                    local content = "\""..pandoc.utils.stringify(c["contents"]).."\""
-                    table.insert(flatifiedrow,content)
+                    local content = pandoc.utils.stringify(c["contents"])
+                    table.insert(flatifiedrow,escape(content))
                 end
                 table.insert(flatified,table.concat(flatifiedrow,","))
           end
@@ -18,16 +34,14 @@ function Writer (doc, opts)
             for j,r in ipairs(v["body"]) do
                 local flatifiedrow = {}
                 for k,c in ipairs(r["cells"]) do
-                    local content = "\""..pandoc.utils.stringify(c["contents"]).."\""
-                    table.insert(flatifiedrow,content)
+                    local content = pandoc.utils.stringify(c["contents"])
+                    table.insert(flatifiedrow,escape(content))
                 end
                 table.insert(flatified,table.concat(flatifiedrow,","))
+                
             end
             
-          end
-          local cb = pandoc.CodeBlock(table.concat(flatified,"\n"))
-          cb["attr"]["classes"] = {"csv"}
-          return cb
+          end          
       end,
       Para = function(t) 
          return ""
@@ -37,7 +51,14 @@ function Writer (doc, opts)
       end,
       BulletList = function(l) 
         return {}
+     end,
+     Header = function(c) 
+        local content = pandoc.utils.stringify(c)
+        table.insert(flatified,"\"\"\n"..escape(content))
+        return {}
      end
     }
-    return pandoc.write(doc:walk(filter), 'markdown', opts)
-  end
+    res = doc:walk(filter)
+    local raw = table.concat(flatified,"\n")
+    return raw
+end
